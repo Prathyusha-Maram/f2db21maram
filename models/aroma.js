37fb7c57a951cdf6159c311c57d35372625a9ea3
Prathyusha-Maram <s548821@nwmissouri.edu>
2022-11-30 13:57:07 -0600

    added files

--- models/aroma.js
+++ models/aroma.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const aromaSchema = mongoose.Schema({ 
- aroma_name: String, 
- brand: String, 
- price: Number 
+ aroma_name: {type: String,required: [true, 'Mobile type cannot be empty']},
+ brand: {type: String,required: [true, 'Mobile size cannot be empty']},
+ price: {type: Number,required: [true, 'Mobile cost cannot be empty']},
 }) 
  
 module.exports = mongoose.model("Aroma", aromaSchema)
--- routes/aroma.js
+++ routes/aroma.js
@@ -2,6 +2,16 @@ var express = require('express');
 const aroma_controlers= require('../controllers/aroma');
 var router = express.Router();
  
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
 
 /* GET aroma */
 router.get('/', aroma_controlers.aroma_view_all_Page );
@@ -10,12 +20,12 @@ router.get('/', aroma_controlers.aroma_view_all_Page );
 router.get('/detail', aroma_controlers.aroma_view_one_Page); 
 
 /* GET create aroma page */ 
-router.get('/create', aroma_controlers.aroma_create_Page); 
+router.get('/create',secured, aroma_controlers.aroma_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', aroma_controlers.aroma_update_Page); 
+router.get('/update',secured, aroma_controlers.aroma_update_Page); 
 
-/* GET delete costume page */ 
-router.get('/delete', aroma_controlers.aroma_delete_Page); 
- 
+/* GET delete aroma page */ 
+router.get('/delete',secured, aroma_controlers.aroma_delete_Page); 
+
 module.exports = router;
