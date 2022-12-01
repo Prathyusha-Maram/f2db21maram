688edac5c4bd7bfa73e4f949e222a373520da6a5
Prathyusha-Maram <s548821@nwmissouri.edu>
2022-11-30 23:59:29 -0600

    modified files

--- models/aroma.js
+++ models/aroma.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const aromaSchema = mongoose.Schema({ 
- aroma_name: {type: String,required: [true, 'Mobile type cannot be empty']},
- brand: {type: String,required: [true, 'Mobile size cannot be empty']},
- price: {type: Number,required: [true, 'Mobile cost cannot be empty']},
+ aroma_name: {type: String,required: [true, 'Aroma name cannot be empty']},
+ brand: {type: String,required: [true, 'Aroma brand cannot be empty']},
+ price: {type: Number,required: [true, 'Aroma price cannot be empty']},
 }) 
  
 module.exports = mongoose.model("Aroma", aromaSchema)
